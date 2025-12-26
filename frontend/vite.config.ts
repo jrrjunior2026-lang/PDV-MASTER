47cce8a260d9997a07594931c0aacd119a11f168
jrrjunior25 <firetv2023tcl@gmail.com>
2025-12-26 01:07:08 -0300

    tudo ok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PDV-MASTER/',
   plugins: [react()],
   build: {
     target: 'es2015',
